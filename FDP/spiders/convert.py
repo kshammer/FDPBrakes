77e606c48c1383417f437d10a204dafb0157909d
kshammer <hammerschmiks@vcu.edu>
2018-05-07 00:46:34 -0400

    changed orielly to hardcode values

--- FDP/spiders/convert.py
+++ FDP/spiders/convert.py
@@ -1,6 +1,6 @@
 import csv
 output = open("out.txt", "w")
-with open('NAPA.csv', newline="") as f:
+with open('reilly.csv', newline="") as f:
     for row in f:
-        row = "\"" + row[:-1] + "\"" + "," 
+        row = "\"" + row[:-2] + "\"" + "," 
         output.write(row)
